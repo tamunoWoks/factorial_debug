31ed968776417cd8f962fdaf153f76d1a2002d56
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-08-25 17:47:08 -0600

    import, configure & start logging system

--- factorial_debug_logger.py
+++ factorial_debug_logger.py
@@ -1,1 +1,11 @@
+# Import the logging module for debugging and tracking program execution
+import logging
 
+# Configure the logging system:
+logging.basicConfig(
+    level=logging.DEBUG, # captures all messages (DEBUG and above)
+    format='%(asctime)s -  %(levelname)s -  %(message)s' #specifies what details to show in each log message
+)
+
+# Log the start of the program
+logging.debug('Start of program')
