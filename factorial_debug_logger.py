2766de58bd4b40e60adfb5d15ea9a5227872fbfe
Tamunosaki Osima Wokoma <tamunosakiwokoma86@gmail.com>
2025-08-25 17:47:59 -0600

    Compute factorial & log intermediate steps

--- factorial_debug_logger.py
+++ factorial_debug_logger.py
@@ -9,3 +9,19 @@ logging.basicConfig(
 
 # Log the start of the program
 logging.debug('Start of program')
+
+def factorial(n):
+    """
+    Compute factorial of a number n using iteration,
+    while logging intermediate steps for debugging.
+    """
+    logging.debug('Start of factorial(' + str(n) + ')')
+    total = 1  # Initialize the product result
+    
+    # Loop through numbers from 0 up to n (inclusive)
+    for i in range(n + 1):
+        total *= i  # Multiply total by the current i
+        logging.debug('i is ' + str(i) + ', total is ' + str(total))  # Log current state
+    
+    logging.debug('End of factorial(' + str(n) + ')')
+    return total  # Return the computed factorial
